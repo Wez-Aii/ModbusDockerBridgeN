59918963ad563988f350d61ab0e5fbf7a8e1b121
Wez Aii <wez.aiindustries@gmail.com>
2024-12-11 03:32:53 +0000

    Save code commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # ModbusDockerBridgeN
 A testing repo for establishing modbus conn to plc device within the host network from Docker Container attatched to bridge network instead of host network
+
+# TO Create a docker network that enable to communicate with other device in host network
+docker network create --driver bridge \
+  --subnet=172.16.0.0/16 \
+  --gateway=172.16.0.1 \
+  --opt "com.docker.network.bridge.enable_ip_masquerade=true" \
+  --opt "com.docker.network.bridge.host_binding_ipv4=0.0.0.0" \
+  --opt "com.docker.network.bridge.enable_icc=true" \
+  lan-bridge
---
+++ main2.py
@@ -0,0 +1,22 @@
+from pymodbus.client import ModbusTcpClient
+from pymodbus.exceptions import ConnectionException
+import os
+
+# PLC_ADDR = os.getenv("PLC_ADDR", "localhost")
+PLC_ADDR = "192.168.1.5"
+PLC_MODBUS_PORT = 502
+
+
+if __name__ == "__main__":
+    _plc_addr = PLC_ADDR
+    _plc_port = PLC_MODBUS_PORT
+    mc_conn = ModbusTcpClient(host=_plc_addr, port=_plc_port)
+    try:
+        is_connect = mc_conn.connect()
+        if mc_conn.is_socket_open():
+            result = mc_conn.read_holding_registers(
+                address=3, count=3, slave=1
+            )
+            print(result)
+    except ConnectionException as e:
+        print(f"AFTER Fail to connected to Modbus TCP server == {mc_conn}.")
